0496daee2bf839f5d37092f898e7e38e40c3f666
Pluswick <tjdgks0814@gmail.com>
2024-11-26 19:20:08 +0900

    Separate project from CP1
    Created repository for project "Capitalism"

--- Project/Capitalism/Header/ClearScreen.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLEARSCREEN_H
-#define CLEARSCREEN_H
-
-#include "Player.h" // Player 구조체 정의 포함
-
-void originalClearScreen();
-void modifiedClearScreen(Player player, int balance);
-
-#endif // CLEARSCREEN_H
